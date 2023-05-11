08f0eda54d119d7cdaa0f7816365f28b15180ad3
Osman Fırat <osmanfrat@yahoo.com>
2023-05-11 18:17:25 +0300

    Colliders and random animal spawner added

--- pizza-shooter-code/Assets/Scripts/DestroyOutOfBounds.cs
+++ pizza-shooter-code/Assets/Scripts/DestroyOutOfBounds.cs
@@ -11,10 +11,11 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        }
-        else if(transform.position.z < lowerBound)
+        } else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
+        
     }
 }
---
+++ pizza-shooter-code/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other) {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
