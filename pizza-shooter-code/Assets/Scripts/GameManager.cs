309470ba2407c57ead65be854c13bd39d47637b9
Osman Fırat <osmanfrat@yahoo.com>
2023-05-13 14:14:30 +0300

    Added animal hnger bars, score, live UI and executable game file

--- pizza-shooter-code/Assets/Scripts/GameManager.cs
+++ pizza-shooter-code/Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private int score = 0;
     private int lives = 3;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI liveText;
     void Start()
     {
         
@@ -27,11 +31,15 @@ public class GameManager : MonoBehaviour
             lives = 0;
         }
         Debug.Log("Lives = " + lives);
+        liveText.text = "Lives: " + lives;
+
     }
 
     public void AddScore(int value)
     {
         score += value;
         Debug.Log("Score = " + score);
+        scoreText.text = "Score: " + score;
+
     }
 }
